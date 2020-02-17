c7e6b67de990fd7ba35f99606df1b475875720a8
Frithjof Winkelmann <frithjof97@web.de>
2020-02-17 23:06:14 +0100

    Add option to test model

---
+++ src/test_model.py
@@ -0,0 +1,46 @@
+import gym
+from models.policy import FFPolicy
+from models.value import FFValue
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import gym
+
+from util.replay_buffer import ReplayBuffer
+
+from util.rl_board import RLBoard
+
+mse = nn.MSELoss()
+
+def exec_policy(env, policy):
+    observation = env.reset()
+
+    for i in range(100000):
+        env.render()
+
+        state = torch.tensor(observation).float()
+
+        act = policy(state.reshape((1,-1)))
+
+        action = torch.argmax(act)
+        observation, _, d, _ = env.step(action.item())
+
+        if d:
+            observation = env.reset()
+
+
+def main():
+    env = gym.make('CartPole-v0')
+
+    actions = env.action_space.n
+    inp = env.observation_space.shape[0]
+
+    policy = FFPolicy(input_size=inp, actions=actions)
+    policy.load_state_dict(torch.load('policy.pth'))
+    policy = policy.eval()
+
+    exec_policy(env, policy)
+
+
+main()
