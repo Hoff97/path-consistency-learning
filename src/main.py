c6cda3f3f9a9b2b0eb873056f3d88b8819ee3edc
Frithjof Winkelmann <frithjof97@web.de>
2020-02-18 14:53:34 +0100

    Support environments with discrete observation spaces

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import gym
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from gym.spaces.discrete import Discrete
+from gym.spaces.tuple import Tuple
 from models.policy import FFPolicy
 from models.value import FFValue
 from util.replay_buffer import ReplayBuffer
@@ -11,27 +13,30 @@ from util.rl_board import RLBoard
 
 mse = nn.MSELoss()
 
+
 def exec_policy(env, policy, seq_len, epsilon):
-    states = torch.zeros(seq_len+1, env.observation_space.shape[0])
+    states = torch.zeros(seq_len+1, observation_dim(env))
     actions = torch.zeros(seq_len, dtype=torch.long)
     rewards = torch.zeros(seq_len)
     done = torch.zeros(seq_len)
 
-    total_len = 0
+    total_len = None
 
     observation = env.reset()
 
     for i in range(seq_len):
-        states[i] = torch.tensor(observation)
+        states[i] = state_enc(env, observation)
 
         action = None
         if random.random() > epsilon:
             act = policy(states[i].reshape((1,-1)))
             action = torch.argmax(act).item()
         else:
-            action = env.action_space.sample()
+            sample = env.action_space.sample()
+            action = encode_action(env, sample)
         actions[i] = action
-        observation, reward, d, info = env.step(action)
+        decoded = decode_action(env, action)
+        observation, reward, d, info = env.step(decoded)
 
         rewards[i] = reward
         done[i] = d
@@ -39,13 +44,69 @@ def exec_policy(env, policy, seq_len, epsilon):
             total_len = i
             break
 
-    states[total_len+1] = torch.tensor(observation)
+    if total_len is None:
+        total_len = seq_len-1
+    states[total_len+1] = state_enc(env, observation)
 
     return states, actions, rewards, done, total_len
 
 
+def observation_dim(env):
+    if isinstance(env.observation_space, Discrete):
+        return env.observation_space.n
+    else:
+        return env.observation_space.shape[0]
+
+
+def one_hot(dim, n):
+    res = torch.zeros(dim)
+    res[n] = 1
+    return res
+
+
+def encode_action(env, action):
+    if isinstance(env.action_space, Tuple):
+        act_code = 0
+        k = 1
+        for i, a in enumerate(env.action_space):
+            act_code += k*action[i]
+            k *= a.n
+        return act_code
+    else:
+        return action
+
+
+def product(ls):
+    k = 1
+    for l in ls:
+        k *= l
+    return k
+
+
+def decode_action(env, act_code):
+    if isinstance(env.action_space, Tuple):
+        action = []
+        num_prods = len(env.action_space)
+        k = product([a.n for a in env.action_space[0:-1]])
+        for i in range(num_prods):
+            act = act_code // k
+            action.insert(0, act)
+            act_code = act_code % k
+            if i < num_prods - 1:
+                k = k // env.action_space[num_prods - i - 2].n
+        return tuple(action)
+    else:
+        return act_code
+
+
+def state_enc(env, observation):
+    if isinstance(env.observation_space, Discrete):
+        return one_hot(env.observation_space.n, observation)
+    else:
+        return torch.tensor(observation)
+
 def exec_policy_batch(env, policy, seq_len, batches, epsilon):
-    states = torch.zeros(batches, seq_len+1, env.observation_space.shape[0])
+    states = torch.zeros(batches, seq_len+1, observation_dim(env))
     actions = torch.zeros(batches, seq_len, dtype=torch.long)
     rewards = torch.zeros(batches, seq_len)
     done = torch.zeros(batches, seq_len)
@@ -63,15 +124,21 @@ def exec_policy_batch(env, policy, seq_len, batches, epsilon):
 
 
 def main():
-    env = gym.make('CartPole-v0')
+    env = gym.make('Acrobot-v1')
 
-    actions = env.action_space.n
-    inp = env.observation_space.shape[0]
-    batch_size = 10
-    seq_len = 200
+    actions = 1
+    if isinstance(env.action_space, Tuple):
+        for a in env.action_space:
+            actions *= a.n
+    else:
+        actions = env.action_space.n
+    inp = observation_dim(env)
+    batch_size = 1
+    seq_len = 1000
     lamb = 1
     tau = 1
     epsilon = 1
+    e_decay = 0.99
 
     device = 'cpu'
 
@@ -85,7 +152,7 @@ def main():
     optim_p = optim.Adam(policy.parameters())
     optim_v = optim.Adam(value.parameters())
 
-    buffer = ReplayBuffer(capacity=100)
+    buffer = ReplayBuffer(capacity=500)
 
     board = RLBoard()
 
@@ -124,7 +191,7 @@ def main():
             print('Saving models')
             save_models(policy, value)
 
-        epsilon = epsilon*0.99
+        epsilon = epsilon*e_decay
 
 
 def save_models(policy, value):
